9b2ae06757541c553fffb0d1e3f39dece8e0860f
jdmasa <47989615+jdmasa@users.noreply.github.com>
2025-10-07 15:14:44 +0200

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Target, Menu, X } from 'lucide-react';
 import { Language } from '../types';
 import { t } from '../translations';
 import { useState } from 'react';
+import capnLogo from '/capn.svg';
 
 interface HeaderProps {
   language: Language;
@@ -28,8 +29,7 @@ export default function Header({ language, onLanguageChange, onNavigate }: Heade
       <div className="container mx-auto px-4 py-4">
         <div className="flex justify-between items-center">
           <div className="flex items-center space-x-3 cursor-pointer" onClick={() => onNavigate('home')}>
-
-            <img src="capn.svg" alt="CAPN Logo" />
+            <img src={capnLogo} alt="CAPN Logo" className="w-[75px]" />
             <div>
               <h1 className="text-2xl font-bold">{t('clubName', language)}</h1>
               <p className="text-xs text-slate-300">Barcelona</p>
