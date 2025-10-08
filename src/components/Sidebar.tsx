acace4e719e82f77ce12c839006ca818f70146ba
jdmasa <47989615+jdmasa@users.noreply.github.com>
2025-10-08 18:30:38 +0200

    Updated Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -61,6 +61,7 @@ export default function Sidebar({ language, onNavigate }: SidebarProps) {
       onDataFetch={handleDataFetch}
       onError={handleError}
          maxRedirects={5}
+            language
     />
         </div>
       </div>
